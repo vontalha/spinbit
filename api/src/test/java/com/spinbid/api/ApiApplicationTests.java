bd283e920ce73bb80db986d8702d0d8a28e6ecb1
talha <Talhaooa@gmail.com>
2025-05-12 22:05:16 +0200

    Initial commit from spinbid root

---
+++ api/src/test/java/com/spinbid/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spinbid.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
